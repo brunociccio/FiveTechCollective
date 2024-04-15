02bb793a26e8a7bd3cd2da4b03f83b4dce042754
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-04-15 18:40:25 -0300

    Ultimas alterações no Application '@controller"

--- fivetechcolletive/src/main/java/br/com/fiap/fivetechcolletive/FivetechcolletiveApplication.java
+++ fivetechcolletive/src/main/java/br/com/fiap/fivetechcolletive/FivetechcolletiveApplication.java
@@ -2,10 +2,12 @@ package br.com.fiap.fivetechcolletive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class FivetechcolletiveApplication {
 
     public static void main(String[] args) {
