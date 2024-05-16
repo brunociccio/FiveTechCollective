c912de710edabf7a6493359a2b4e2cb167837b39
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-05-16 11:50:13 -0300

    instalação da dependencia da Oracle DB / criação das creds relacionadas ao DB nos arquivos de properties

--- fivetechcolletive/src/main/java/br/com/fiap/fivetechcolletive/config/DataBaseSeeder.java
+++ fivetechcolletive/src/main/java/br/com/fiap/fivetechcolletive/config/DataBaseSeeder.java
@@ -1,6 +1,8 @@
 package br.com.fiap.fivetechcolletive.config;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -12,7 +14,7 @@ import br.com.fiap.fivetechcolletive.repository.EnderecoRepository;
 
 @Configuration
 @Profile("dev")
-public class DataBaseSeeder {
+public class DataBaseSeeder implements CommandLineRunner {
 
 @Autowired
 CadastroRepository cadastroRepository;
@@ -29,5 +31,30 @@ DocumentosRepository documentosRepository;
 
 @Autowired
 EnderecoRepository enderecoRepository;
+
+@Override
+public void run(String... args) throws Exception {
+
+    cadastroRepository.saveAll(
+        List.of()
+    );  
+    
+    cadastrarCnpjRepository.saveAll(
+        List.of()
+    );
+
+    contatoRepository.saveAll(
+        List.of()
+    );
+
+    documentosRepository.saveAll(
+        List.of()
+    );
+
+    enderecoRepository.saveAll(
+        List.of()
+    );
+
+}
     
 }
--- fivetechcolletive/src/main/java/br/com/fiap/fivetechcolletive/repository/CadastroRepository.java
+++ fivetechcolletive/src/main/java/br/com/fiap/fivetechcolletive/repository/CadastroRepository.java
@@ -1,14 +1,8 @@
 package br.com.fiap.fivetechcolletive.repository;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import br.com.fiap.fivetechcolletive.config.DataBaseSeeder;
 import br.com.fiap.fivetechcolletive.model.Cadastro;
 
 @Repository
